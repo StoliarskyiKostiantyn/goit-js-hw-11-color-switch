d4897e0c5ce69a81c055bfe722ac273e4687462c
Костя Столярский <73879155+StoliarskyiKostiantyn@users.noreply.github.com>
2021-05-22 21:57:28 +0300

    Writing on data sets

--- index.js
+++ index.js
@@ -3,9 +3,8 @@
 const randomIntegerFromInterval = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1) + min);
 };
-
+const buttons = document.querySelectorAll("button[data-action]");
 const bodeEl = document.querySelector("body");
-
 const colors = [
   "#FFFFFF",
   "#2196F3",
@@ -19,4 +18,25 @@ const getNumber = randomIntegerFromInterval(0, 5);
 const chooseColor = (colors, getNumber) => {
   bodeEl.style.backgroundColor = colors[getNumber];
 };
-chooseColor(colors, getNumber);
+// chooseColor(colors, getNumber);
+
+buttons.forEach((button) => {
+  button.addEventListener("click", onBtnClick);
+});
+let intervalId = null;
+let isActive = false;
+function onBtnClick(ev) {
+  const { action } = ev.currentTarget.dataset;
+  switch (action) {
+    case "start":
+      if (isActive) {
+        return;
+      } else {
+        isActive = true;
+        intervalId = setInterval(chooseColor, 1000, 1000);
+      }
+    case "stop":
+      clearInterval(intervalId);
+      isActive = false;
+  }
+}
